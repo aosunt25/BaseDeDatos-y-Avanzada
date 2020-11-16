6d637eefbbea5433f67df539409760695b038244
Alfredo Osuna Torres <38539148+aosunt25@users.noreply.github.com>
2020-11-16 15:08:03 -0600

    Primera conexion a redis

--- Proyecto Final BD/api.py
+++ Proyecto Final BD/api.py
@@ -3,27 +3,40 @@ import redis
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 
+#Connection to MongoDB
 client = MongoClient('mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false')
 
 db = client.proyecto_final
 collection = db['movie']
 
-def movieName(movie):
+#Connection to redis DB
+r = redis.Redis(host="localhost", port=6379, db=0)
 
-def actorName(actor):
+#def movieName(movie):
 
-def tvShowName(tvShow):
+#def actorName(actor):
+
+#def tvShowName(tvShow):
 
 def totalNumMovTV():
     cursos = collection.count()
 
 
 def totalMovCountry(country):
-    myquery = [
-    {"$group": {"_id": "$country","total": {"$sum": 1}}},
-    {"$match" :{ "_id":country}}
-    ]
-    cursos = list(collection.aggregate(myquery))
+    try:
+        total = r.get(country).decode("utf-8")
+        print("Redis")
+        print("Total number of movies on ", country, " is ", total)
+    except:
+        print("Mongo")
+        myquery = [
+        {"$group": {"_id": "$country","total": {"$sum": 1}}},
+        {"$match" :{ "_id":country}}
+        ]
+        cursos = list(collection.aggregate(myquery))
+        for doc in cursos:
+            r.set(country, doc["total"])
+            print("Total number of movies on ", country, " is ", doc["total"])
 
 def totalMovieTVperYear(year):
     myquery = [
@@ -33,13 +46,13 @@ def totalMovieTVperYear(year):
     ]
     cursos = list(collection.aggregate(myquery))
 
-def addMovie():
+#def addMovie():
 
-def addTVShow():
+#def addTVShow():
 
 
 menu = 0
-while menu!= 6:
+while menu!= 9:
     print('Menu del Api \n')
     print('1. Give movie name \n')
     print('2. Give actor name \n')
@@ -50,4 +63,9 @@ while menu!= 6:
     print('7. Add new movie \n')
     print('8. Add a new TV show \n')
     print('9. Exit')
+    menu = int(input())
     if menu == 1:
+        print("C")
+    elif menu == 5:
+        country = input("Name of the country\n")
+        totalMovCountry(country)
